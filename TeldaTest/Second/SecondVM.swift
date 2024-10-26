cdfb5b31413b39a0fd49d78073a199b5ce340350
mohamady996 <mohamady996@gmail.com>
2024-10-27 02:48:58 +0300

    get All Cast for ALL similar movies

--- TeldaTest/Second/SecondVM.swift
+++ TeldaTest/Second/SecondVM.swift
@@ -16,6 +16,9 @@ class SecondVM{
     let similarMovies = BehaviorSubject<[SimilarMovieResult]>(value: [])
     private let bag = DisposeBag()
     private var id :Int?
+    
+    var allSimilarMovies: [SimilarMovieResult] = []
+    
     let casts = BehaviorSubject<[Cast]>(value: [])
     var castList: [Cast] = []
     let directors = BehaviorSubject<[Cast]>(value: [])
@@ -31,6 +34,8 @@ class SecondVM{
         self.id = movieID
         fetchMovieDetails()
         fetchSimilarMovies()
+        
+        fetchAllSimilarMovies()
     }
     
     ///Fetches the movie Details
@@ -66,7 +71,7 @@ class SecondVM{
                 
                 self?.similarMovies.onNext( Array( similarMoviesResponse.similarMoviesResults?.prefix(5) ?? [] ))
                 
-                self?.fetchAllCast()
+//                self?.fetchAllCast()
             }, onFailure: { [weak self] apiError in
                 self?.similarMovies.onNext([])
             })
@@ -104,6 +109,67 @@ class SecondVM{
                 self?.casts.onNext([])
             })
             .disposed(by: bag)
+    }
+    
+    private
+    func fetchAllSimilarMovies(){
+        let urlString = "https://api.themoviedb.org/3/movie/\(self.id!)/similar"
+        let dispatchGroup = DispatchGroup()
+        let queryItems = (1...500).map { "\($0)" } // Create different query items
+        let headers = [
+            "accept": "application/json",
+              "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOGVmMGVhNTc3ZDBjYjc4ZDdhYmU2MWRmMGI0MjE2MiIsIm5iZiI6MTcyOTk0ODA0NS40MDcyOTUsInN1YiI6IjY3MWNlMjNlOWZmNjgxZDllMGE0MzE0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PuwiYyegT45tifVnUMlKF-dHLgrGs4yJR57WBSzq8rk"
+            ]
+        
+        for queryItem in queryItems {
+            let queries = [URLQueryItem(name: "page", value: queryItem)]
+            dispatchGroup.enter() // Enter the dispatch group for each request
+            NetworkManager.shared.fetchData(from: urlString, method: .get, headers: headers, queryItems: queries).subscribe(onSuccess: { [weak self] (similarMoviesResponse: SimilarMoviesResponse) in
+                
+                self?.allSimilarMovies.append(contentsOf: similarMoviesResponse.similarMoviesResults ?? [])
+                print("similar movies request completed")
+                dispatchGroup.leave()
+            }, onFailure: { [weak self] apiError in
+                dispatchGroup.leave()
+            }).disposed(by: bag)
+        }
+        
+        // Notify when all requests are complete
+        dispatchGroup.notify(queue: .main) {
+            print("All similar movies request completed: \(self.allSimilarMovies.count)")
+            self.fetchAllCastForAllSimilarMovies()
+        }
+    }
+    
+    private
+    func fetchAllCastForAllSimilarMovies(){
+        let dispatchGroup = DispatchGroup()
+        var urls: [String] = []
+        let headers = [
+            "accept": "application/json",
+              "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOGVmMGVhNTc3ZDBjYjc4ZDdhYmU2MWRmMGI0MjE2MiIsIm5iZiI6MTcyOTk0ODA0NS40MDcyOTUsInN1YiI6IjY3MWNlMjNlOWZmNjgxZDllMGE0MzE0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PuwiYyegT45tifVnUMlKF-dHLgrGs4yJR57WBSzq8rk"
+            ]
+        for movie in allSimilarMovies {
+            let url = "https://api.themoviedb.org/3/movie/\(movie.id ?? 0)/credits"
+        
+            dispatchGroup.enter() // Enter the dispatch group for each request
+            NetworkManager.shared.fetchData(from: url, method: .get, headers: headers).subscribe(onSuccess: { [weak self] (fetchCastResponse: FetchCastResponse) in
+                
+                self?.castList.append(contentsOf: fetchCastResponse.cast ?? [])
+                self?.directorsList.append(contentsOf: fetchCastResponse.crew ?? [])
+                print("cast request completed")
+                dispatchGroup.leave()
+            }, onFailure: { [weak self] apiError in
+                dispatchGroup.leave()
+            }).disposed(by: bag)
+        }
+        
+        // Notify when all requests are complete
+        dispatchGroup.notify(queue: .main) {
+            print("All cast request completed: \(self.allSimilarMovies.count)")
+            self.casts.onNext(self.castList ?? [])
+            self.directors.onNext(self.directorsList ?? [])
+        }
     }
 
 }
